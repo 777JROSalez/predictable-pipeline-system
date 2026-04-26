1e86aa89b8be213823c1283bbe44635b3fd1e4ee
777JROSalez <tylertejral@gmail.com>
2026-04-26 08:36:08 -0500

    install meta pixel

--- pps-app/app/layout.tsx
+++ pps-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -37,11 +38,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* 🔥 FORCE META TAG (THIS IS THE IMPORTANT PART) */}
         <meta
           name="facebook-domain-verification"
           content="25chd9zvo24fmy52jfvq8buvd8al1m"
         />
+
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2140288869880172');
+            fbq('track', 'PageView');
+          `}
+        </Script>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -56,6 +71,16 @@ export default function RootLayout({
       </head>
 
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2140288869880172&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <a href="#main-content" className="skip-nav">
           Skip to main content
         </a>
